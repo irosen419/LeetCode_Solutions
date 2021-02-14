0b7e2304e1df688d0c33e29b8363fb1f5678f1d4
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-02-14 18:38:17 -0500

    Time: 88 ms (74.55%), Space: 39.1 MB (92.31%) - LeetHub

--- trapping-rain-water/trapping-rain-water.js
+++ trapping-rain-water/trapping-rain-water.js
@@ -3,19 +3,27 @@
  * @return {number}
  */
 var trap = function(height) {
+    // edge case: if there are no heights, return 0
     if (!height.length) return 0
+    
+    // initialize variables sum, wallHeight, maxHeight, heightIndex
     let sum = 0
     let wallHeight = 0
-    let maxHeight = height.indexOf(Math.max(...height))
-    for (let i = 0; i <= maxHeight; i++) {
-        if (height[i] > wallHeight) {
-            wallHeight = height[i]
-        } else {
-            sum += wallHeight - height[i]
+    let maxHeight = height[0]
+    let heightIndex = 0
+    
+    // find the index of the highest height in height array
+    for (let i = 0; i < height.length; i++) {
+        if (height[i] > maxHeight) {
+            maxHeight = height[i]
+            heightIndex = i
         }
     }
-    wallHeight = 0
-    for (let i = height.length - 1; i >= maxHeight; i--) {
+    
+    // loop over heights until the highest index
+    // if current height is taller than wallHeight, wallHeight = current height
+    // at each index, find the difference between the current height and the wallHeight
+    for (let i = 0; i <= heightIndex; i++) {
         if (height[i] > wallHeight) {
             wallHeight = height[i]
         } else {
@@ -23,5 +31,16 @@ var trap = function(height) {
         }
     }
     
+    // reset wallHeight
+    // repeat above loop in opposite direction until you reach the highest index
+    wallHeight = 0
+    for (let i = height.length - 1; i >= heightIndex; i--) {
+        if (height[i] > wallHeight) {
+            wallHeight = height[i]
+        } else {
+            sum += wallHeight - height[i]
+        }
+    }
+
     return sum
 };
