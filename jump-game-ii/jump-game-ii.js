b88c0dded7784b8c03518046d130f51bfdfe61a3
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-02-21 13:51:59 -0500

    Time: 84 ms (65.51%), Space: 40 MB (27.85%) - LeetHub

--- jump-game-ii/jump-game-ii.js
+++ jump-game-ii/jump-game-ii.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var jump = function(nums) {
-    if (nums.length === 1) {
-        return 0
-    }
+    if (nums.length == 1) return 0
+    
     let slow = 0
     let fast = 0
     let nextPossibleMax = 0
     let jumps = 0
-    
     
     while (fast < nums.length - 1) {
         while (slow <= fast) {
             nextPossibleMax = Math.max(nextPossibleMax, slow + nums[slow])
             slow++
         }
+        
         fast = nextPossibleMax
         jumps++
     }
