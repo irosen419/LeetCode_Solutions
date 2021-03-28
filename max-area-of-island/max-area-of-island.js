7afab3db4d5d196d26d8f3d23e0b2dd105fb26a9
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-03-28 19:59:24 -0400

    Time: 108 ms (47.70%), Space: 45.7 MB (19.00%) - LeetHub

--- max-area-of-island/max-area-of-island.js
+++ max-area-of-island/max-area-of-island.js
@@ -3,40 +3,36 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
+    let maxArea = 0
     let dirs = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-    
-    let maxCount = 0
     
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === 1) {
-                search([[i, j]])
+                calculator([[i, j]])
             }
         }
     }
     
-    return maxCount
+    return maxArea
     
-    function search(queue) {
-
-        let currentMax = 0
-        while (queue.length) {
-
-            let [row, col] = queue.shift()
-            
-            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) {
+    function calculator(stack) {
+        let currentArea = 0
+        
+        while(stack.length) {
+            let [row, col] = stack.pop()
+            if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] === 0) {
                 continue
             }
             
             grid[row][col] = 0
-            currentMax++
+            currentArea++
             
             for (const dir of dirs) {
-                queue.push([row + dir[0], col + dir[1]])
-            }
-            
+                stack.push([row + dir[0], col + dir[1]])
+            } 
         }
         
-        maxCount = Math.max(currentMax, maxCount)
+        maxArea = Math.max(maxArea, currentArea)
     }
 };
