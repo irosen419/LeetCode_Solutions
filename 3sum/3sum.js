1666c586addddce1306bd195188e068347d11657
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-04-04 17:26:52 -0400

    Time: 160 ms (52.10%), Space: 48.9 MB (58.91%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -8,19 +8,21 @@ var threeSum = function(nums) {
     let res = []
     
     nums.sort((a, b) => a - b)
+    // [-1,0,1,2,-1,-4] => [-4,-1,-1,0,1,2]
+    
     for (let i = 0; i < nums.length - 2; i++) {
         let j = i + 1
         let k = nums.length - 1
         
-        if (i !== 0 && nums[i] === nums[i-1]) continue
-
+        if ( i !== 0 && nums[i] === nums[i - 1]) continue
+        
         while (j < k) {
             let sum = nums[i] + nums[j] + nums[k]
-
+            
             if (sum === 0) {
                 res.push([nums[i], nums[j], nums[k]])
                 j++
-                while (j < k && nums[j] == nums[j-1]) ++j
+                while(j < k && nums[j] === nums[j - 1]) ++j
             } else if (sum < 0) {
                 j++
             } else {
@@ -30,5 +32,4 @@ var threeSum = function(nums) {
     }
     
     return res
-    
 };
