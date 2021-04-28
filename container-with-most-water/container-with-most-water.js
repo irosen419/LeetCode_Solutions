94cab277433f4787f4319a7ad7a7bd88576be973
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-04-28 19:29:24 -0400

    Time: 96 ms (45.39%), Space: 48.1 MB (41.98%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -3,20 +3,29 @@
  * @return {number}
  */
 var maxArea = function(height) {
+    if (height.length < 2) return 0
+    if (height.length === 2) return Math.min(...height)
+    
     let i = 0
     let j = height.length - 1
     let total = 0
     
-    while (i < j) {
-        let min
-        if (height[i] < height[j]) {
-            min = height[i]
-            total = Math.max(total, min * (j - i))
+    while (i <= j) {
+        
+        if (height[i] <= height[j]) {
+            let min = Math.min(height[i], height[j])
+            let area = min * (j - i)
             i++
+            if (area >= total) {
+                total = area
+            }
         } else {
-            min = height[j]
-            total = Math.max(total, min * (j - i))
+            let min = Math.min(height[i], height[j])
+            let area = min * (j - i)
             j--
+            if (area >= total) {
+                total = area
+            }
         }
     }
     
