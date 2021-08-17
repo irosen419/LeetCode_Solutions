c096695fca4cc058451c559e7ed04f3770d1d5db
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2021-08-17 14:06:51 -0400

    Time: 72 ms (72.30%), Space: 39.9 MB (47.60%) - LeetHub

--- search-insert-position/search-insert-position.js
+++ search-insert-position/search-insert-position.js
@@ -4,20 +4,11 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let start = 0
-    let end = nums.length - 1
-    let mid
-    
-    while (start <= end) {
-        mid = Math.floor((start+end)/2)
-        if (target < nums[mid]) {
-            end = mid - 1
-        } else if (target > nums[mid]) {
-            start = mid + 1
-        } else {
-            return mid
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i
         }
     }
     
-    return start
+    return nums.length
 };
