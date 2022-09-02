e9727c7e73804d01e29f7b0a9a198cac61da603c
Ian Rosen <61032240+irosen419@users.noreply.github.com>
2022-09-01 22:55:05 -0500

    Time: 235 ms (78.34%), Space: 57.2 MB (85.56%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
@@ -4,10 +4,6 @@
  * @return {number}
  */
 var garbageCollection = function(garbage, travel) {
-    let G = 0
-    let P = 0
-    let M = 0
-    
     let driveTime = 0;
     
     let lastIndices = [-1, -1, -1]
@@ -16,17 +12,16 @@ var garbageCollection = function(garbage, travel) {
         
         for (let j = 0; j < garbage[i].length; j++) {
             if (garbage[i][j] === 'G') {
-                G += 1
                 lastIndices[0] = i
             }
             if (garbage[i][j] === 'P') {
-                P += 1
                 lastIndices[1] = i
             }
             if (garbage[i][j] === 'M') {
-                M += 1
                 lastIndices[2] = i
             }
+            
+            driveTime += 1
         }
     }
     
@@ -37,5 +32,5 @@ var garbageCollection = function(garbage, travel) {
             j++
         }
     }
-    return G + M + P + driveTime
+    return driveTime
 };
